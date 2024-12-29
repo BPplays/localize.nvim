53012e18b4db67a22868f3659ad6b3531cab6276
BPplays <andymalbp@gmail.com>
2024-12-29 00:47:58 -0800

    init

--- lua/localize/init.lua
+++ lua/localize/init.lua
@@ -1,9 +1,11 @@
-Localize = {}
-Localize.langmap = {}
-Localize.langmap.builtin = {}
-Localize.current_lang = "en_US"
+local localize = {}
+localize.langmap = {}
+localize.langmap.builtin = {}
+localize.options = {
+  language = "en_US"
+}
 
-Localize.langmap.builtin.ja_JP = {
+localize.langmap.builtin.ja_JP = {
 	['normal<mode>']      = 'ノーマル',
 	['insert<mode>']      = '挿入',
 	['visual<mode>']      = 'ビジュアル',
@@ -17,61 +19,69 @@ Localize.langmap.builtin.ja_JP = {
 	['shell<mode>']      = '端末',
 }
 
-Localize.langmap.builtin = {
-	['ja_JP']      = Localize.langmap.builtin.ja_JP,
-	['ja_JP.utf8']      = Localize.langmap.builtin.ja_JP,
+localize.langmap.builtin = {
+	['ja_JP']      = localize.langmap.builtin.ja_JP,
+	['ja_JP.utf8']      = localize.langmap.builtin.ja_JP,
 }
-Localize.bankmap = {
-	['builtin'] = Localize.langmap.builtin,
-	['internal'] = Localize.langmap.builtin
+localize.bankmap = {
+	['builtin'] = localize.langmap.builtin,
+	['internal'] = localize.langmap.builtin
 }
-Localize.protected = {
+localize.protected = {
 	['global'] = true,
 	['builtin'] = true,
 	['internal'] = true,
 }
 
-function Localize.set_bank(bank, bank_value)
-	if Localize.protected[bank] then
+function localize.set_bank(bank, bank_value)
+	if localize.protected[bank] then
 		return false
 	end
-	Localize.bankmap[bank] = bank_value
+	localize.bankmap[bank] = bank_value
 	return true
 end
 
-function Localize.remove_bank(bank)
-	if Localize.protected[bank] then
+function localize.remove_bank(bank)
+	if localize.protected[bank] then
 		return false
 	end
-	Localize.bankmap[bank] = nil
+	localize.bankmap[bank] = nil
 	return true
 end
 
-function Localize.get_string(string, lang, bank)
-	bank = bank or 'global'
-	lang = lang or Localize.current_lang
+function localize.get_string(string, lang, bank_or_banks)
+	bank_or_banks = bank_or_banks or 'global'
+	lang = lang or localize.options.language
+	local banks = {}
 
+	if type(bank_or_banks) == "string" then
+		banks = { bank_or_banks }
+	else
+		banks = bank_or_banks
+	end
 
-	if bank == 'global' then
-		for _, bank_map in pairs(Localize.bankmap) do
+	for _, bank in ipairs(banks) do
+		if bank == 'global' then
+			for _, bank_map in pairs(localize.bankmap) do
+				local lang_map = bank_map[lang]
+				if lang_map ~= nil then
+					if lang_map[string] ~= nil then
+						return lang_map[string]
+					end
+
+				end
+
+			end
+		else
+			local bank_map = localize.bankmap[bank]
 			local lang_map = bank_map[lang]
 			if lang_map ~= nil then
 				if lang_map[string] ~= nil then
 					return lang_map[string]
+
 				end
 
 			end
-
-		end
-	else
-		local bank_map = Localize.bankmap[bank]
-		local lang_map = bank_map[lang]
-		if lang_map ~= nil then
-			if lang_map[string] ~= nil then
-				return lang_map[string]
-
-			end
-
 		end
 	end
 	return nil
@@ -101,8 +111,8 @@ local function get_no_helper_str(str)
     return new_str
 end
 
-function Localize.get_string_or_orig_no_helper(string, lang, bank)
-	local retstr = Localize.get_string(string, lang, bank)
+function localize.get_string_or_orig_no_helper(string, lang, bank_or_banks)
+	local retstr = localize.get_string(string, lang, bank_or_banks)
 	if type(retstr) == "string" then
 		return retstr
 		-- return retstr.gsub(retstr, '([^\\])([<>])', '%1[substituted]')
@@ -112,3 +122,10 @@ function Localize.get_string_or_orig_no_helper(string, lang, bank)
 	end
 	return nil
 end
+
+function localize.setup(opts)
+	localize.options = vim.tbl_deep_extend("force", localize.options, opts or {})
+end
+
+return localize
+
